5987573b1272243b45b50ccc450f0ca8911c4dbb
vagrant <Dcameroncheng@gmail.com>
2020-04-10 03:53:51 +0000

    added more functionality

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -22,4 +22,20 @@ fetchISSFlyOverTimes({ latitude: '43.63190', longitude: '79.37160' }, (error, pa
     return;
   }
   console.log('It worked! Returned ISS Flyover Times' , passTimes);
+});
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+
+  console.log(passTimes);
 });
